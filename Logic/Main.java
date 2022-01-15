3ea831e18b73c05bc7f84b50e33d6f7b1c038322
Tony <tnd.lamquang@student.avans.nl>
2022-01-15 21:32:44 +0100

    Commited Main w/ launch GUI

---
+++ Logic/Main.java
@@ -0,0 +1,12 @@
+package Programmeren2.Logic;
+
+import Programmeren2.Gui.Gui;
+import javafx.application.Application;
+
+public class Main{
+
+public static void main(String[] args) {
+    Application.launch(Gui.class);
+}
+    
+}
