d4b1ea8b81e2b4aea4168d371a2ae0f4eec6587a
Tony <tnd.lamquang@student.avans.nl>
2022-04-10 17:29:04 +0200

    Added constructor and getValue()

--- Domain/Difficulty.java
+++ Domain/Difficulty.java
@@ -1,8 +1,20 @@
 package Programmeren2.Domain;
 
 public enum Difficulty {
-    BEGINNER,
-    INTERMEDIATE,
-    EXPERT,
-    NONE,
+    BEGINNER("Beginner"),
+    INTERMEDIATE("Intermediate"),
+    EXPERT("Expert"),
+    NONE("None"),;
+
+    private String diff;
+
+    private Difficulty(String diff) {
+        this.diff = diff;
+    }
+
+    public String getValue() {
+        return this.diff;
+    }
+
+    
 }
