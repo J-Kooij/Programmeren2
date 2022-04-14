eb44b2dc4ee50dd2128076796ecd5252a782b84c
Jonah Eikenhorst <jonah.eikenhorst@gmail.com>
2022-04-14 17:37:28 +0200

    Added GUI classes for Student&CreateStudent

---
+++ Gui/Student/GStudent.java
@@ -0,0 +1,17 @@
+package Gui.Student;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class GStudent {
+    public static void showWindow(Stage window) {
+
+    }
+}
