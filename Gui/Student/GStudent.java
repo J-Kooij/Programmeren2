27fc729887b1ca40a62130d661a39668df6d04c2
Jonah Eikenhorst <jonah.eikenhorst@gmail.com>
2022-04-15 01:37:14 +0200

    Added standard buttons to Student

--- Gui/Student/GStudent.java
+++ Gui/Student/GStudent.java
@@ -1,11 +1,16 @@
 package Programmeren2.Gui.Student;
 
+import Programmeren2.Gui.Gui;
+import Programmeren2.Gui.Course.GCourse;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -13,9 +18,23 @@ import javafx.stage.Stage;
 public class GStudent {
     public static void showWindow(Stage window) {
         window.setTitle("CodeCademy | Students");
-        VBox vLayout = new VBox();
+        BorderPane Layout = new BorderPane();
+        Label label1 = new Label("test");
 
-        Scene scene = new Scene(vLayout);
+        Layout.getChildren().addAll(label1);
+
+        //Setting layout for buttons at bottom
+        HBox lHBox = new HBox();
+        Layout.setBottom(lHBox);
+        Button backButton = new Button("< Back");
+        backButton.setOnAction(e -> {GCourse.showWindow(window);;});
+        Button createButton = new Button("Create");
+        Button editButton = new Button("Edit");
+        Button deleteButton = new Button("Delete");
+        
+        lHBox.setSpacing(10);
+        lHBox.getChildren().addAll(backButton, createButton, editButton, deleteButton);
+        Scene scene = new Scene(Layout, 500, 250);
         window.setScene(scene);
     }
 }
