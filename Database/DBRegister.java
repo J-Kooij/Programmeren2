323948a6771b02daf1372cd49c96031ccc076b0f
Tony <tnd.lamquang@student.avans.nl>
2022-04-15 21:33:11 +0200

    Created DBRegister class with empty methods

---
+++ Database/DBRegister.java
@@ -0,0 +1,24 @@
+package Programmeren2.Database;
+
+public class DBRegister extends Database{
+
+    public DBRegister() {
+        super();
+    }
+    
+    public void createRegistration() {
+
+    }
+
+    public void viewRegistration() {
+
+    }
+
+    public void updateRegistration() {
+
+    }
+
+    public void deleteRegistration() {
+
+    }
+}
