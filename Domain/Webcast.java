1f8128e3d3cefc1ea8682fd9b0cd64d2165938e7
Tony <tnd.lamquang@student.avans.nl>
2022-04-15 13:08:15 +0200

    Added getters, setters and constructor

--- Domain/Webcast.java
+++ Domain/Webcast.java
@@ -1,5 +1,23 @@
 package Programmeren2.Domain;
 
 public class Webcast {
-
+    private int contentItemId;
+    private Speaker speaker;
+    public Webcast(int contentItemId, Speaker speaker) {
+        this.contentItemId = contentItemId;
+        this.speaker = speaker;
+    }
+    public int getContentItemId() {
+        return contentItemId;
+    }
+    public void setContentItemId(int contentItemId) {
+        this.contentItemId = contentItemId;
+    }
+    public Speaker getSpeaker() {
+        return speaker;
+    }
+    public void setSpeaker(Speaker speaker) {
+        this.speaker = speaker;
+    }
+    
 }
