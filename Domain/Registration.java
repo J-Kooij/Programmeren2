2bdcb794f0789636ba7287f42ab3d2024e0073e9
Tony <tnd.lamquang@student.avans.nl>
2022-04-16 20:59:03 +0200

    Removed certificate

--- Domain/Registration.java
+++ Domain/Registration.java
@@ -4,13 +4,11 @@ import java.sql.Date;
 
 public class Registration {
     private Date registrationDate;
-    private int certificateId;
     private Course course;
     private Student student;
 
-    public Registration(Date registrationDate, int certificateId, Course course, Student student) {
+    public Registration(Date registrationDate, Course course, Student student) {
         this.registrationDate = registrationDate;
-        this.certificateId = certificateId;
         this.course = course;
         this.student = student;
     }
@@ -20,12 +18,6 @@ public class Registration {
     }
     public void setRegistrationDate(Date registrationDate) {
         this.registrationDate = registrationDate;
-    }
-    public int getCertificateId() {
-        return certificateId;
-    }
-    public void setCertificateId(int certificateId) {
-        this.certificateId = certificateId;
     }
     public Course getCourse() {
         return course;
