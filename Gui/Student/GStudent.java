3a146d1980b1eb86a67d1f185b26f49d3ef223ef
Jonah Eikenhorst <jonah.eikenhorst@gmail.com>
2022-04-17 17:40:55 +0200

    New graphical design for table

--- Gui/Student/GStudent.java
+++ Gui/Student/GStudent.java
@@ -33,22 +33,22 @@ public class GStudent {
         column1.prefWidthProperty().bind(tableView.widthProperty().divide(4));
 
         tableView.setStyle("-fx-background-color: linear-gradient(to bottom, #9CC0E7, #EEEEEE); -fx-background-radius: 7px 7px 0px 0px; -fx-padding: 0 0 5px 0;");
-        column1.setStyle("-fx-background-color: #9CC0E7;");
+        column1.setStyle("-fx-background-color: linear-gradient(to bottom, #9CC0E7, #EEEEEE);");
 
         TableColumn<Student, String> column2 = new TableColumn<>("Email");
         column2.setCellValueFactory(new PropertyValueFactory<>("Email"));
         column2.prefWidthProperty().bind(tableView.widthProperty().divide(4));
-        column2.setStyle("-fx-background-color: #9CC0E7;");
+        column2.setStyle("-fx-background-color: linear-gradient(to bottom, #9CC0E7, #EEEEEE);");
 
         TableColumn<Student, String> column3 = new TableColumn<>("Gender");
         column3.setCellValueFactory(new PropertyValueFactory<>("Gender"));
         column3.prefWidthProperty().bind(tableView.widthProperty().divide(4));
-        column3.setStyle("-fx-background-color: #9CC0E7;");
+        column3.setStyle("-fx-background-color: linear-gradient(to bottom, #9CC0E7, #EEEEEE);");
 
         TableColumn<Student, String> column4 = new TableColumn<>("Birthdate");
         column4.setCellValueFactory(new PropertyValueFactory<>("BirthDate"));
         column4.prefWidthProperty().bind(tableView.widthProperty().divide(4));
-        column4.setStyle("-fx-background-color: #9CC0E7;");
+        column4.setStyle("-fx-background-color: linear-gradient(to bottom, #9CC0E7, #EEEEEE);");
 
 
 
