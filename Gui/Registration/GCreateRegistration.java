6c36852aed10800e49ba562915c1cbb0f9750afb
Tony <tnd.lamquang@student.avans.nl>
2022-04-17 20:02:03 +0200

    Implemented the createRegistration button

--- Gui/Registration/GCreateRegistration.java
+++ Gui/Registration/GCreateRegistration.java
@@ -1,7 +1,15 @@
 package Programmeren2.Gui.Registration;
 
 import javafx.stage.Stage;
+
+import java.util.List;
+
+import Programmeren2.Database.DBCourse;
+import Programmeren2.Database.DBRegister;
+import Programmeren2.Domain.Course;
+import Programmeren2.Domain.Registration;
 import Programmeren2.Domain.Student;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
@@ -9,20 +17,29 @@ import javafx.scene.layout.VBox;
 
 public class GCreateRegistration {
     public static void showWindow(Stage window, Student student) {
+        DBRegister dbRegister = new DBRegister();
         VBox vBox = new VBox();
         Label title = new Label("Sign" + student.getName() + "in for a course");
         Label email = new Label("Email: " + student.getEmail());
         ChoiceBox<String> courseChoice = new ChoiceBox<>();
-        // Moet loop komen die alle courses add vanuit arraylist
-        // niet vergeten type naar Course veranderen
-        courseChoice.getItems().add("TestCourse1");
-        courseChoice.getItems().add("TestCourse2");
-        courseChoice.getItems().add("TestCourse3");
+
+        DBCourse dbCourse = new DBCourse();
+        List<Course> courses = dbCourse.getCourses();
+        for (Course c : courses) {
+            courseChoice.getItems().add(c.getCourseName());
+        }
 
         Button signIn = new Button("Save & Sign student in");
         signIn.setStyle(
                 "-fx-background-color: #0495bd; -fx-text-fill: white; -fx-font-size:17; -fx-font-weight: bold");
 
-        vBox.getChildren().addAll(title, email, courseChoice, signIn);
+        signIn.setOnAction((event) ->{
+            Registration newRegistration = new Registration(  new java.sql.Date(System.currentTimeMillis())  , dbCourse.getCourse(courseChoice.getValue()), student);
+            dbRegister.createRegistration(newRegistration);
+            GRegistration.showWindow(window, student);
+        });
+                vBox.getChildren().addAll(title, email, courseChoice, signIn);
+        Scene scene = new Scene(vBox, 500, 300);
+        window.setScene(scene);
     }
 }
