37ee2c07eff918e9e7d123b39590da2780a97413
Jonah Eikenhorst <jonah.eikenhorst@gmail.com>
2022-04-17 02:01:42 +0200

    Made Registration GUI class

---
+++ Gui/Student/GRegistration.java
@@ -0,0 +1,99 @@
+package Programmeren2.Gui.Student;
+
+import javafx.event.EventHandler;
+import Programmeren2.Domain.Student;
+import Programmeren2.Gui.Gui;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class GRegistration {
+    public static void showWindow(Stage window, Student student){ 
+        BorderPane layout = new BorderPane();
+        VBox vBox = new VBox();
+        Label title = new Label("Student: xxx");
+        title.setPadding(new Insets(0, 0, 5, 0));
+        vBox.setAlignment(Pos.CENTER);
+        title.setStyle("-fx-font-weight: bold; -fx-font-size:25");
+
+        Label subtitle = new Label("Registrations of Student:");
+        subtitle.setStyle("-fx-font-weight: bold; -fx-font-size:17");
+        layout.setTop(vBox);
+        vBox.getChildren().addAll(title, subtitle);
+
+
+        TableView<Student> tableView = new TableView<>();
+        layout.setCenter(tableView);
+
+        TableColumn<Student, String> column1 = new TableColumn<>("Course name:");
+        column1.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        column1.prefWidthProperty().bind(tableView.widthProperty().divide(3));
+
+        TableColumn<Student, String> column2 = new TableColumn<>("Student:");
+        column2.setCellValueFactory(new PropertyValueFactory<>("Email"));
+        column2.prefWidthProperty().bind(tableView.widthProperty().divide(3));
+
+        TableColumn<Student, String> column3 = new TableColumn<>("Registration Date: ");
+        column3.setCellValueFactory(new PropertyValueFactory<>("Gender"));
+        column3.prefWidthProperty().bind(tableView.widthProperty().divide(3));
+
+        tableView.getColumns().add(column1);
+        tableView.getColumns().add(column2);
+        tableView.getColumns().add(column3);
+
+        // for(Student s : students){
+        //     tableView.getItems().add(s);
+        // }
+        HBox lHBox = new HBox();
+        layout.setBottom(lHBox);
+        Button backButton = new Button("< Back");
+        backButton.setOnAction(e -> {try {
+            Gui.showWindow(window);
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        };});
+        Button createButton = new Button("Create");
+        createButton.setOnAction(e -> {GCreateStudent.showWindow(window);});
+        Button editButton = new Button("Edit");
+        Button deleteButton = new Button("Delete");
+        Button infoButton = new Button("🛈 More info");
+        lHBox.setMargin(infoButton, new Insets(0, 0, 0, 225));
+
+        //Pop-up for more information about the student
+        infoButton.setOnAction(
+            new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    final Stage dialog = new Stage();
+                    dialog.initModality(Modality.APPLICATION_MODAL);
+                    dialog.initOwner(window);
+                    VBox dialogVbox = new VBox(20);
+                    dialogVbox.getChildren().add(new Text("This is a Dialog"));
+                    Scene dialogScene = new Scene(dialogVbox, 300, 200);
+                    dialog.setScene(dialogScene);
+                    dialog.show();
+                }
+             });
+        
+        lHBox.setSpacing(10);
+        lHBox.getChildren().addAll(backButton, createButton, editButton, deleteButton, infoButton);
+        Scene scene = new Scene(layout, 550, 350);
+        window.setScene(scene);
+
+
+    }
+}
